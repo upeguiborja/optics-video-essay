0eca6ee8aeaf506073d8f4d4f8dcb682798964d0
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-03-29 08:06:20 -0500

    Update Fresnel scene and add Polarizers scene

--- start.py
+++ start.py
@@ -1,16 +1,16 @@
-from manimlib import *
+from manim import *
 import numpy as np
 
 
 class Q1Intro(Scene):
-  question = Text(
-      r'''1. Explicar el significado físico de los coeficientes de Fresnel para interfaces
-  entre medios dieléctricos. Ilustrarlos considerando las interfaces
-  aire (n=1.0) - vidrio (n=1.5) y vidrio - aire''')
 
   def construct(self):
-    self.question.scale(0.5)
-    self.play(Write(self.question, run_time=5))
+    question = Text(
+        r'''1. Explicar el significado físico de los coeficientes de Fresnel para interfaces
+  entre medios dieléctricos. Ilustrarlos considerando las interfaces
+  aire (n=1.0) - vidrio (n=1.5) y vidrio - aire''')
+    question.scale(0.5)
+    self.play(Write(question, run_time=5))
 
 
 class Q2Intro(Q1Intro):
@@ -38,15 +38,90 @@ AXES_CONFIG = {
 }
 
 
-class Snell(Scene):
+class Fresnel(ThreeDScene):
 
   def construct(self):
-    self.camera.frame.set_euler_angles(phi=75 * DEGREES, theta=45 * DEGREES)
-    axes = ThreeDAxes(**AXES_CONFIG)
-    prism = Prism(dimensions=[3, 2, 1])
-    prism.set_opacity(0.3)
-    prism.move_to([0, 0, 0])
+    self.set_camera_orientation(
+        phi=75 * DEGREES,
+        theta=10 * DEGREES,
+    )
 
-    self.add(axes)
-    self.add(prism)
+    axes = ThreeDAxes(**AXES_CONFIG)
+    prism = Prism(dimensions=[2, 10, 2])
+    prism.set_opacity(0.5)
+    prism.move_to(np.array([0, 0, -1]))
+
+    self.play(
+        FadeIn(axes, run_time=2),
+        Write(prism, run_time=2),
+    )
+
+    self.wait(0)
+
+    incident_ray = Line(
+        start=np.array([0, -10, 10]),
+        end=ORIGIN,
+        color=RED,
+    )
+
+    transmitted_ray = Line(
+        start=ORIGIN,
+        end=np.array([0, 10 * np.sin(0.490883), -10 * np.cos(0.490883)]),
+        color=GREEN,
+    )
+
+    reflected_ray = Line(
+        start=ORIGIN,
+        end=np.array([0, 10, 10]),
+        color=BLUE,
+    )
+
+    self.play(FadeIn(incident_ray, run_time=2))
+    self.play(
+        FadeIn(transmitted_ray, run_time=2),
+        FadeIn(reflected_ray, run_time=2),
+    )
     self.wait()
+
+
+class Polarized(ThreeDScene):
+
+  def construct(self):
+    self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
+    polarizer_body = Square(fill_opacity=0.25, color=BLUE)
+
+    polarizer_arrow = Line(start=np.array([0, 0, 0]),
+                           end=np.array([0, 1, 0]),
+                           stroke_color=RED)
+
+    polarizer_1 = VGroup(polarizer_body, polarizer_arrow)
+    polarizer_1.rotate(90 * DEGREES, axis=X_AXIS)
+    polarizer_1.move_to(np.array([0, PI, 0]))
+
+    polarizer_2 = polarizer_1.copy()
+    polarizer_2.move_to(np.array([0, 0, 0]))
+    polarizer_2.rotate(45 * DEGREES, axis=Y_AXIS)
+    polarizer_2[1].set_stroke(color=BLUE)
+
+    polarizer_3 = polarizer_1.copy()
+    polarizer_3.move_to(np.array([0, -PI, 0]))
+    polarizer_3.rotate(90 * DEGREES, axis=Y_AXIS)
+
+    e_field = ParametricFunction(
+        lambda t: np.array([0, t, np.sin(3 * t)]),
+        t_min=-PI,
+        t_max=0,
+        color=YELLOW,
+    )
+
+    axes = ThreeDAxes()
+
+    self.play(
+        Write(polarizer_1, run_time=2),
+        Write(polarizer_2, run_time=2),
+        Write(polarizer_3, run_time=2),
+        FadeIn(axes, run_time=2),
+    )
+    self.begin_ambient_camera_rotation()
+    self.add(e_field)
+    self.wait(10)
