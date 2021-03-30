d479021d3b3391e4fda784b783ff892aa490013e
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-03-29 22:12:58 -0500

    Add light beams

--- f_1_10.py
+++ f_1_10.py
@@ -6,18 +6,18 @@ class Intro(Scene):
 
   def construct(self):
     question = Text(
-        r'''2. Explicar las leyes de reflexión y refracción desde las perspectivas
+        r'''F1.2 Explicar las leyes de reflexión y refracción desde las perspectivas
     de las ondas electromagnéticas. A partir de la ley de refracción,
     discutir la dispersión de la luz en un prisma''')
-
     question.scale(0.5)
+
     self.play(Write(question, run_time=5))
+    self.wait(5)
 
 
 class OpticalPrism(Scene):
 
   def construct(self):
-    theta = ValueTracker(10 * DEGREES)
     prism = Triangle(
         fill_opacity=0.25,
         color="WHITE",
@@ -26,15 +26,121 @@ class OpticalPrism(Scene):
     prism.scale(3)
     vertices = prism.get_vertices()
 
-    center = midpoint(vertices[0], vertices[1])
+    center_1 = midpoint(vertices[0], vertices[1])
 
     self.play(Write(prism))
 
     incident_ray = Line(
         start=(-10, -2, 0),
-        end=center,
+        end=center_1,
         stroke_width=5,
     )
 
     self.play(Write(incident_ray))
+    self.wait(2)
+
+    center_2 = midpoint(vertices[0], vertices[2])
+
+    grow = midpoint(vertices[0], vertices[2]) - vertices[0]
+    diff_ray_1 = Line(
+        start=center_1,
+        end=center_2,
+        stroke_width=5,
+        color=RED,
+    )
+
+    diff_ray_2 = Line(
+        start=center_1,
+        end=diff_ray_1.end + grow * 0.1,
+        stroke_width=5,
+        color=YELLOW,
+    )
+
+    diff_ray_3 = Line(
+        start=center_1,
+        end=diff_ray_1.end + grow * 0.2,
+        stroke_width=5,
+        color=GREEN,
+    )
+
+    diff_ray_4 = Line(
+        start=center_1,
+        end=diff_ray_1.end + grow * 0.3,
+        stroke_width=5,
+        color=BLUE_C,
+    )
+
+    diff_ray_5 = Line(
+        start=center_1,
+        end=diff_ray_1.end + grow * 0.4,
+        stroke_width=5,
+        color=BLUE_E,
+    )
+
+    diff_ray_6 = Line(
+        start=center_1,
+        end=diff_ray_1.end + grow * 0.5,
+        stroke_width=5,
+        color=PURPLE,
+    )
+
+    self.play(
+        Write(diff_ray_1),
+        Write(diff_ray_2),
+        Write(diff_ray_3),
+        Write(diff_ray_4),
+        Write(diff_ray_5),
+        Write(diff_ray_6),
+    )
     self.wait()
+
+    end_ray_1 = Line(
+        start=diff_ray_1.end,
+        end=(10, -2, 0),
+        stroke_width=5,
+        color=RED,
+    )
+
+    end_ray_2 = Line(
+        start=diff_ray_2.end,
+        end=end_ray_1.end + DOWN * 0.5,
+        stroke_width=5,
+        color=YELLOW,
+    )
+
+    end_ray_3 = Line(
+        start=diff_ray_3.end,
+        end=end_ray_1.end + DOWN * 1,
+        stroke_width=5,
+        color=GREEN,
+    )
+
+    end_ray_4 = Line(
+        start=diff_ray_4.end,
+        end=end_ray_1.end + DOWN * 1.5,
+        stroke_width=5,
+        color=BLUE_C,
+    )
+
+    end_ray_5 = Line(
+        start=diff_ray_5.end,
+        end=end_ray_1.end + DOWN * 2,
+        stroke_width=5,
+        color=BLUE_E,
+    )
+
+    end_ray_6 = Line(
+        start=diff_ray_6.end,
+        end=end_ray_1.end + DOWN * 2.5,
+        stroke_width=5,
+        color=PURPLE,
+    )
+
+    self.play(
+        Write(end_ray_1),
+        Write(end_ray_2),
+        Write(end_ray_3),
+        Write(end_ray_4),
+        Write(end_ray_5),
+        Write(end_ray_6),
+    )
