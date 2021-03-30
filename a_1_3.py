948f0770ab7a3e9f699dcbd02ad3267135e26a26
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-03-29 19:22:15 -0500

    Update polarizer scene

--- a_1_3.py
+++ a_1_3.py
@@ -3,14 +3,20 @@ import numpy as np
 
 
 class Intro(Scene):
-  question = Text(
-      r'''3. La siguiente figura muestra un montaje experimental de tres polarizadores
-  lineales. Los ejes de transmisión del polarizador cercano a la vela y del analizador
-  son mutualmente ortogonales. mientras que el del polarizador del medio
-  hace 45º con el del polarizador cercano a la vela.''')
+
+  def construct(self):
+    question = Text(
+        r'''3. La siguiente figura muestra un montaje experimental de tres polarizadores
+    lineales. Los ejes de transmisión del polarizador cercano a la vela y del analizador
+    son mutualmente ortogonales. mientras que el del polarizador del medio
+    hace 45º con el del polarizador cercano a la vela.''')
+    question.scale(0.5)
+
+    self.play(Write(question))
+    self.wait()
 
 
-class Polarized(ThreeDScene):
+class Polarizer(ThreeDScene):
 
   def construct(self):
     self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
@@ -33,6 +39,11 @@ class Polarized(ThreeDScene):
     polarizer_3.move_to(np.array([0, -PI, 0]))
     polarizer_3.rotate(90 * DEGREES, axis=Y_AXIS)
 
+    e_unpolarized = Cylinder(height=PI,
+                             direction=(0, 1, 0),
+                             fill_color=YELLOW,
+                             fill_opacity=0.5)
+
     e_field = ParametricFunction(
         lambda t: np.array([0, t, np.sin(3 * t)]),
         t_min=-PI,
@@ -42,12 +53,12 @@ class Polarized(ThreeDScene):
 
     axes = ThreeDAxes()
 
-    self.play(
-        Write(polarizer_1, run_time=2),
-        Write(polarizer_2, run_time=2),
-        Write(polarizer_3, run_time=2),
-        FadeIn(axes, run_time=2),
-    )
-    self.begin_ambient_camera_rotation()
-    self.add(e_field)
+    # self.play(
+    #     Write(polarizer_1, run_time=2),
+    #     Write(polarizer_2, run_time=2),
+    #     Write(polarizer_3, run_time=2),
+    #     FadeIn(axes, run_time=2),
+    # )
+    # self.begin_ambient_camera_rotation()
+    self.add(e_unpolarized, polarizer_1, polarizer_2, polarizer_3, axes)
     self.wait(10)
